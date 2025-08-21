2b9ae86c28ba764f2b92a8c6d294d70eba8a3823
007slm <007slm@163.com>
2025-08-22 01:05:08 +0800

    主要添加ojp dashboard

--- docker/mysql/initdb.d/init-permissions.sql
+++ docker/mysql/initdb.d/init-permissions.sql
@@ -1,3 +1,7 @@
 -- 授予 smartcache 用户 REPLICATION CLIENT 和 REPLICATION SLAVE 权限
 GRANT REPLICATION CLIENT, REPLICATION SLAVE ON *.* TO 'smartcache'@'%';
+
+-- 授予 smartcache 用户创建和管理数据库(schema)的权限
+GRANT CREATE, ALTER, DROP, INDEX, CREATE VIEW, SHOW VIEW ON *.* TO 'smartcache'@'%';
+
 FLUSH PRIVILEGES;

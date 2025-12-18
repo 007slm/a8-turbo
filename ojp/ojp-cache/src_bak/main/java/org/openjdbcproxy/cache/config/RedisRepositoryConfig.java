4364c66265b672dbe582857bba59974b7149bedd
007slm <007slm@163.com>
2025-12-19 00:01:39 +0800

    refactor: 移除旧缓存模块备份文件，并调整项目配置、依赖及文档

--- ojp/ojp-cache/src_bak/main/java/org/openjdbcproxy/cache/config/RedisRepositoryConfig.java
+++
@@ -1,9 +0,0 @@
-package org.openjdbcproxy.cache.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-
-@Configuration
-@EnableRedisRepositories(basePackages = "org.openjdbcproxy.cache.repository")
-public class RedisRepositoryConfig {
-}
--- ojp/ojp-cache/src_bak/main/java/org/openjdbcproxy/cache/repository/MyCacheRuleRepository.java
+++
@@ -1,9 +0,0 @@
-package org.openjdbcproxy.cache.repository;
-
-import org.openjdbcproxy.cache.entity.CacheRule;
-import org.springframework.data.repository.ListCrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MyCacheRuleRepository extends ListCrudRepository<CacheRule, String> {
-}
